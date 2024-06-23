09fb7f114f3f83cf94aeb074a3abc3c3f6e8e514
anj107 <angelivrivera@iskolarngbayan.pup.edu.ph>
2024-06-23 14:10:42 +0800

    Added minimize, maximize and close button

--- src/main/java/com/example/sqlinserttest/AdminLoginController.java
+++ src/main/java/com/example/sqlinserttest/AdminLoginController.java
@@ -9,13 +9,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class AdminLoginController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private ImageView closebtn;
+
+    @FXML
+    private ImageView mnmbtn;
+
+    @FXML
+    private ImageView mxmbtn;
 
     @FXML
     private Button loginButton;
@@ -25,6 +35,27 @@ public class AdminLoginController {
 
     @FXML
     private TextField userTextfield;
+
+    @FXML
+    void closeclick(MouseEvent event) {
+        javafx.application.Platform.exit();
+    }
+
+    @FXML
+    void mnmclick(MouseEvent event) {
+        Stage stage = (Stage) mnmbtn.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    void mxmclick(MouseEvent event) {
+        Stage stage = (Stage) mxmbtn.getScene().getWindow();
+        if (stage.isMaximized()){
+            stage.setMaximized(false);
+        }else {stage.setMaximized(true);
+    }
+    }
+
     public void switchToAdmin1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("admin.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
